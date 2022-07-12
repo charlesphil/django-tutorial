2e917d17da5affcc08c9e137c9b1054913b08392
Charles Phil <charlesphil@CharlesPhilsMBP.attlocal.net>
2022-07-12 00:20:48 -0400

    initial commit

---
+++ django_project/blog/views.py
@@ -0,0 +1,28 @@
+from turtle import title
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 11, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 12, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
